55ab05481c6c93b9ed32b1e9f3de8260034e6164
Michelle Li <45521335+floatingturnip@users.noreply.github.com>
2020-11-21 14:28:54 -0800

    initial frames

--- App.js
+++ App.js
@@ -1,11 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Button, TouchableOpacity, Alert } from 'react-native';
+import Background from './assets/loginPageBlue.png';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const image = require('./assets/loginPageBlue.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={image} style={styles.image}>
+      <TouchableOpacity
+      onPress={() => Alert.alert('Top button pressed')}>
+          <View 
+            style={{
+              alignItems: "center",
+              backgroundColor: 'grey', 
+              opacity: 0.5,
+              padding: 10,
+              marginBottom:150,
+              marginTop: 350
+            }} 
+          >
+            <Text>My Button</Text>
+          </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+      onPress={() => Alert.alert('Bottun button pressed')}>
+          <View 
+            style={{
+              "alignItems": "flex-start",
+              "paddingStart": 40,
+              "paddingTop": 5,
+              "width": 200,
+              "height": 44,
+              marginBottom: 150,
+              backgroundColor: 'grey', 
+              opacity: 0.5,
+            }} 
+          >
+            <Text>My Button</Text>
+          </View>
+        </TouchableOpacity>
+
+      <TouchableOpacity
+      onPress={() => Alert.alert('Bottun button pressed')}>
+          <View 
+            style={{
+              "alignItems": "flex-start",
+              "paddingStart": 40,
+              "paddingTop": 5,
+              "width": 200,
+              "height": 44,
+              backgroundColor: 'grey', 
+              opacity: 0.5,
+            }} 
+          >
+            <Text>My Button</Text>
+          </View>
+      </TouchableOpacity>
+
+        
+      </ImageBackground>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +74,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    //alignItems: 'center',
     justifyContent: 'center',
+    
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+
+  },
+  text: {
+    color: 'grey',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+    
   },
 });
