268f3745829d16ea35b888e7a3f074266a25d340
alexi <a.garbuz@alumni.ubc.ca>
2020-11-22 01:50:34 -0800

    final commit

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { ImageBackground, StyleSheet, Text, View, Button, TouchableOpacity, Alert, TextInput } from 'react-native';
+import { TouchableHighlight, ImageBackground, StyleSheet, Text, View, Button, TouchableOpacity, Alert, TextInput, Switch } from 'react-native';
 import Background from './assets/loginPageBlue.png';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 
 import * as Progress from 'react-native-progress';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 //import MaterialIcon from 'material-icons-react';
 //import { Icon } from 'react-native-elements';
 import { Ionicons } from '@expo/vector-icons';
+import { ViewWeek } from '@material-ui/icons';
 //import Icon from 'react-native-vector-icons/MaterialIcons';
 //import CommunityIcon from 'react-native-icons/MaterialCommunityIcons'
 
@@ -18,11 +20,8 @@ const user = {
   name: "Michael",
   goalsAchieved: 6,
   goalsToGo: 4,
-  //goal: ["French", "Workout"],
-  goal: [{name:'French', height:200},
-            {name:'Wrokout', height:250},],
-
-  //goalHeight: ["French"=200, "Workout"=250]
+  goal: ["French", "Workout"],
+  
 }
 
 function LoginPages({navigation}){
@@ -84,7 +83,7 @@ function LoginPages({navigation}){
               borderBottomRightRadius: 20,
               borderTopRightRadius: 20,
             }} >
-              <Text style={{paddingBottom: 8, color: "white", fontSize: 23}}>{user.goal[0]['name']}{"\n"}</Text>
+              <Text style={{paddingBottom: 8, color: "white", fontSize: 23}}>{user.goal[0]}{"\n"}</Text>
               <Progress.Bar progress={0.3} width={300} height={10} color='#5F97AA' unfilledColor="#EAF9FE"/>
       </View>
       <View style={{
@@ -104,7 +103,7 @@ function LoginPages({navigation}){
               borderBottomRightRadius: 20,
               borderTopRightRadius: 20,
             }} >
-              <Text style={{paddingBottom: 8, color: "white", fontSize: 23}}>{user.goal[1]['name']}{"\n"}</Text>
+              <Text style={{paddingBottom: 8, color: "white", fontSize: 23}}>{user.goal[1]}{"\n"}</Text>
               <Progress.Bar progress={0.8} width={300} height={10} color='#5F97AA' unfilledColor="#EAF9FE"/>
       </View>
         
@@ -118,59 +117,7 @@ function LoginPages({navigation}){
 function DetailsScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <ImageBackground source={require('./assets/AboutPage.png')} style={styles.image}>
-      <TouchableOpacity
-      onPress={() => console.log(2)}>
-          <View 
-            style={{
-              alignItems: "center",
-              backgroundColor: 'grey', 
-              opacity: 0,
-              padding: 10,
-              marginBottom:150,
-              marginTop: 240
-            }} 
-          >
-            <Text>My Button</Text>
-          </View>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-      onPress={() => console.log(1)}>
-          <View 
-            style={{
-              "alignItems": "flex-start",
-              "paddingStart": 40,
-              "paddingTop": 5,
-              "width": 200,
-              "height": 44,
-              marginBottom: 130,
-              backgroundColor: 'grey', 
-              opacity: 0,
-            }} 
-          >
-            <Text>My Button</Text>
-          </View>
-        </TouchableOpacity>
-
-      <TouchableOpacity
-      onPress={() => navigation.navigate('Calendar')}>
-          <View 
-            style={{
-              "alignItems": "flex-start",
-              "paddingStart": 40,
-              "paddingTop": 5,
-              "width": 200,
-              "height": 44,
-              backgroundColor: 'grey', 
-              opacity: 0.5,
-              marginBottom: 70,
-            }} 
-          >
-            <Text>My Button</Text>
-          </View>
-      </TouchableOpacity>
-
+      <ImageBackground source={require('./assets/About_2.png')} style={styles.image}>
         
       </ImageBackground>
       
@@ -192,9 +139,22 @@ function Calendar({ navigation }) {
 }
 
 function Goals({ navigation }) {
+  const [isEnabled, setIsEnabled] = React.useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  const [isEnabled2, setIsEnabled2] = React.useState(false);
+  const toggleSwitch2 = () => setIsEnabled2(previousState => !previousState);
+
+  const [isEnabled3, setIsEnabled3] = React.useState(false);
+  const toggleSwitch3 = () => setIsEnabled3(previousState => !previousState);
   return (
+    
     <View style={styles.container}>
-      <ImageBackground source={require('./assets/goals_1.png')} style={styles.image}>
+      <ImageBackground source={require('./assets/Goals_1.png')} style={styles.image}>
+      
+  
+
+
       <Text style={{position:'absolute',
           left:55,
           top:'8%',
@@ -203,20 +163,18 @@ function Goals({ navigation }) {
           color: "#FFFFFF"}}
           >My Goals</Text>
       
-      {user.goal.map(goal => (
-        
-     
           <View style={{
               position: "absolute",
+              flex: 1,
               alignItems: "left",
-              backgroundColor: "#18363E",
+              backgroundColor: "#5F97AA",
               borderWidth: 0,
-              top: goal['height'],
+              top: 200,
               left: 35,
-              height: 70,
+              height: 100,
               width: 350,
               paddingLeft: 20,
-              paddingTop: 5,
+              paddingTop: 35,
               paddingBottom: 13,
               borderTopLeftRadius: 20,
               borderBottomLeftRadius: 20,
@@ -224,10 +182,89 @@ function Goals({ navigation }) {
               borderTopRightRadius: 20,
             }} >
               <Text style={{paddingBottom: 8, color: "white", fontSize: 23}}>{user.goal[1]}{"\n"}</Text>
-              <Progress.Bar progress={0.8} width={300} height={10} color='#5F97AA' unfilledColor="#EAF9FE"/>
+              
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        //ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+        style={{position: "absolute", top: 35, left: 200}}
+      />
+
+<Text style={{position: "absolute", top:15, left:190}}>Notifications</Text>
+                <Ionicons name="ios-clipboard" color="#18363E" style={{position: "absolute", top: 35, left: 300}} size="30"/>
+                
+           
+      
+    
               </View>
+              <View style={{
+              position: "absolute",
+              flex: 1,
+              alignItems: "left",
+              backgroundColor: "#93C4D1",
+              borderWidth: 0,
+              top: 350,
+              left: 35,
+              height: 100,
+              width: 350,
+              paddingLeft: 20,
+              paddingTop: 35,
+              paddingBottom: 13,
+              borderTopLeftRadius: 20,
+              borderBottomLeftRadius: 20,
+              borderBottomRightRadius: 20,
+              borderTopRightRadius: 20,
+            }} >
+              <Text style={{paddingBottom: 8, color: "white", fontSize: 23}}>Practice piano{"\n"}</Text>
+              
+              <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled2 ? "#f5dd4b" : "#f4f3f4"}
+        //ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch2}
+        value={isEnabled2} style={{position: "absolute", top: 35, left: 200}}
+      />
+      <Text style={{position: "absolute", top:15, left:190}}>Notifications</Text>
+      <Ionicons name="ios-clipboard" color="#18363E" style={{position: "absolute", top: 35, left: 300}} size="30"/>
+    
+              </View>
+              <View style={{
+              position: "absolute",
+              flex: 1,
+              alignItems: "left",
+              backgroundColor: "#3E88A5",
+              borderWidth: 0,
+              top: 500,
+              left: 35,
+              height: 100,
+              width: 350,
+              paddingLeft: 20,
+              paddingTop: 35,
+              paddingBottom: 13,
+              borderTopLeftRadius: 20,
+              borderBottomLeftRadius: 20,
+              borderBottomRightRadius: 20,
+              borderTopRightRadius: 20,
+            }} >
+              <Text style={{paddingBottom: 8, color: "white", fontSize: 23}}>Eat healthier{"\n"}</Text>
+              
+              <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled3 ? "#f5dd4b" : "#f4f3f4"}
+        //ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch3}
+        value={isEnabled3} style={{position: "absolute", top: 35, left: 200}}
+      />
+  
+      <Text style={{position: "absolute", top:15, left:190}}>Notifications</Text>
+      
+    <Ionicons name="ios-clipboard" color="#18363E" style={{position: "absolute", top: 35, left: 300}} size="30"/>
+              </View>   
          
-         ))}
+         
+  
       </ImageBackground>
       
       <StatusBar style="auto" />
@@ -238,7 +275,7 @@ function Goals({ navigation }) {
 function Goals2({ navigation }) {
   return (
     <View style={styles.container}>
-      <ImageBackground source={require('./assets/Goals2.png')} style={styles.image}>
+      <ImageBackground source={require('./assets/NewGoal.png')} style={styles.image}>
         
       </ImageBackground>
       
@@ -263,7 +300,7 @@ export default function App() {
                 : 'ios-home';
             } else if (route.name === 'Goals') {
               iconName = focused ? 'ios-checkbox' : 'ios-checkbox-outline';
-            } else if (route.name === 'Goals2') {
+            } else if (route.name === 'Add Goal') {
                 iconName = focused ? 'ios-add-circle' : 'ios-add-circle-outline';
             } else if (route.name === 'Calendar') {
               iconName = focused ? 'ios-calendar' : 'ios-calendar';
@@ -281,7 +318,7 @@ export default function App() {
         }}>
         <Tab.Screen name="Home" component={LoginPages}  />
         <Tab.Screen name="Goals" component={Goals} />
-        <Tab.Screen name="Goals2" component={Goals2} />
+        <Tab.Screen name="Add Goal" component={Goals2} />
         <Tab.Screen name="Calendar" component={Calendar} />
         <Tab.Screen name="About" component={DetailsScreen} />
       </Tab.Navigator>
