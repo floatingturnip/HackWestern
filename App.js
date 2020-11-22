6d5e80cccd1b728387c48b6df4ab379858751f72
alexi <a.garbuz@alumni.ubc.ca>
2020-11-21 23:23:17 -0800

    alexi changes:) to dashboard and tab bar

--- App.js
+++ App.js
@@ -4,63 +4,105 @@ import { ImageBackground, StyleSheet, Text, View, Button, TouchableOpacity, Aler
 import Background from './assets/loginPageBlue.png';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useRadioGroup } from '@material-ui/core';
+import * as Progress from 'react-native-progress';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//import MaterialIcon from 'material-icons-react';
+//import { Icon } from 'react-native-elements';
+import { Ionicons } from '@expo/vector-icons';
+//import Icon from 'react-native-vector-icons/MaterialIcons';
+//import CommunityIcon from 'react-native-icons/MaterialCommunityIcons'
+
+
+const user = {
+  name: "Michael",
+  goalsAchieved: 6,
+  goalsToGo: 4,
+  goal: ["French", "Workout"]
+}
 
 function LoginPages({navigation}){
   return(
     <View style={styles.container}>
-      <ImageBackground source={require('./assets/loginPageBlue.png')} style={styles.image}>
-      <TouchableOpacity
-      onPress={() => navigation.navigate('Goals')}>
-          <View 
-            style={{
+      <ImageBackground source={require('./assets/dashboard_background_1.png')} style={styles.image}>
+      
+    <Text style={styles.welcome}>Welcome,{"\n"}{user.name}!</Text>
+    <View style={{
+              position: "absolute",
               alignItems: "center",
-              backgroundColor: 'grey', 
-              opacity: 0,
+              backgroundColor: "#18363E",
+              borderWidth: 0,
+              top: 300,
+              left: 40,
+              height: 170,
+              width: 200,
               padding: 10,
-              marginBottom:150,
-              marginTop: 240
-            }} 
-          >
-            <Text>My Button</Text>
-          </View>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-      onPress={() => Alert.alert('Top button pressed')}>
-          <View 
-            style={{
-              "alignItems": "flex-start",
-              "paddingStart": 40,
-              "paddingTop": 5,
-              "width": 200,
-              "height": 44,
-              marginBottom: 130,
-              backgroundColor: 'grey', 
-              opacity: 0,
-            }} 
-          >
-            <Text>My Button</Text>
-          </View>
-        </TouchableOpacity>
-
-      <TouchableOpacity
-      onPress={() => navigation.navigate('About')}>
-          <View 
-            style={{
-              "alignItems": "flex-start",
-              "paddingStart": 40,
-              "paddingTop": 5,
-              "width": 200,
-              "height": 44,
-              backgroundColor: 'grey', 
-              opacity: 0,
-              marginBottom: 70,
-            }} 
-          >
-            <Text>My Button</Text>
-          </View>
-      </TouchableOpacity>
-
+              borderTopLeftRadius: 20,
+              borderBottomLeftRadius: 20,
+              borderBottomRightRadius: 20,
+              borderTopRightRadius: 20,
+            }} >
+              <Text style={{color: "white", fontSize: 23}}>Goals Achieved{"\n"}</Text>
+              <Text style={{color: "white", fontSize: 50}}>{user.goalsAchieved}</Text>
+      </View>
+      <View style={{
+              position: "absolute",
+              alignItems: "center",
+              backgroundColor: "#EAF9FE",
+              borderWidth: 0,
+              top: 350,
+              left: 180,
+              height: 170,
+              width: 200,
+              padding: 10,
+              borderTopLeftRadius: 20,
+              borderBottomLeftRadius: 20,
+              borderBottomRightRadius: 20,
+              borderTopRightRadius: 20,
+            }} >
+              <Text style={{color: "#305F72", fontSize: 23}}>Goals To Go{"\n"}</Text>
+              <Text style={{color: "#305F72", fontSize: 50}}>{user.goalsToGo}</Text>
+      </View>
+      <View style={{
+              position: "absolute",
+              alignItems: "left",
+              backgroundColor: "#18363E",
+              borderWidth: 0,
+              top: 550,
+              left: 40,
+              height: 70,
+              width: 350,
+              paddingLeft: 20,
+              paddingTop: 5,
+              paddingBottom: 13,
+              borderTopLeftRadius: 20,
+              borderBottomLeftRadius: 20,
+              borderBottomRightRadius: 20,
+              borderTopRightRadius: 20,
+            }} >
+              <Text style={{paddingBottom: 8, color: "white", fontSize: 23}}>{user.goal[0]}{"\n"}</Text>
+              <Progress.Bar progress={0.3} width={300} height={10} color='#5F97AA' unfilledColor="#EAF9FE"/>
+      </View>
+      <View style={{
+              position: "absolute",
+              alignItems: "left",
+              backgroundColor: "#18363E",
+              borderWidth: 0,
+              top: 630,
+              left: 40,
+              height: 70,
+              width: 350,
+              paddingLeft: 20,
+              paddingTop: 5,
+              paddingBottom: 13,
+              borderTopLeftRadius: 20,
+              borderBottomLeftRadius: 20,
+              borderBottomRightRadius: 20,
+              borderTopRightRadius: 20,
+            }} >
+              <Text style={{paddingBottom: 8, color: "white", fontSize: 23}}>{user.goal[1]}{"\n"}</Text>
+              <Progress.Bar progress={0.8} width={300} height={10} color='#5F97AA' unfilledColor="#EAF9FE"/>
+      </View>
         
       </ImageBackground>
       
@@ -186,17 +228,43 @@ function Goals2({ navigation }) {
 }
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer >
-      <Stack.Navigator initialRouteName="Home" >
-        <Stack.Screen name="Home" component={LoginPages} />
-        <Stack.Screen name="About" component={DetailsScreen} />
-        <Stack.Screen name="Calendar" component={Calendar} />
-        <Stack.Screen name="Goals" component={Goals} />
-        <Stack.Screen name="Goals2" component={Goals2} />
-      </Stack.Navigator>
+      <Tab.Navigator initialRouteName="Home" screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-home'
+                : 'ios-home';
+            } else if (route.name === 'Goals') {
+              iconName = focused ? 'ios-checkbox' : 'ios-checkbox-outline';
+            } else if (route.name === 'Goals2') {
+                iconName = focused ? 'ios-add-circle' : 'ios-add-circle-outline';
+            } else if (route.name === 'Calendar') {
+              iconName = focused ? 'ios-calendar' : 'ios-calendar';
+            } else if (route.name === 'About') {
+              iconName = focused ? 'ios-settings' : 'ios-settings';
+            }
+          
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })} tabBarOptions={{
+          activeTintColor: '#18363E',
+          inactiveTintColor: '#93C4D1',
+        }}>
+        <Tab.Screen name="Home" component={LoginPages}  />
+        <Tab.Screen name="Goals" component={Goals} />
+        <Tab.Screen name="Goals2" component={Goals2} />
+        <Tab.Screen name="Calendar" component={Calendar} />
+        <Tab.Screen name="About" component={DetailsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
@@ -225,5 +293,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#DDDDDD",
     padding: 10,
     
+  },
+  welcome: {
+    position:'absolute',
+    left:10,
+    top:'10%',
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: "#FFFFFF"
   },
 });
